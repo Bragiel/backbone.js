4145223aae6344e694d7e144bb2ce25a0675ea4d
unknown <gvillanueva@GVILLAPC.salinera.local>
2015-08-11 12:50:25 -0500

    First Push

---
+++ public/js/backbone/models/libro.js
@@ -0,0 +1,14 @@
+var Libro = Backbone.Model.extend({
+    // initialize: function(){
+    //     console.log('HElllllooooou');
+    //
+    //     this.on('change', function(){
+    //         console.log('Model Has been Changed');
+    //     });
+    // },
+    // defaults: {
+    //     autor: 'Desconocido',
+    // }
+
+    urlRoot:'/libros',
+});
